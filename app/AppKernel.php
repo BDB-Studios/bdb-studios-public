f41dad5f3240d1df3af6cce7a4ca45b375aa0faa
Brett Minnie <github@bdb-studios.co.uk>
2015-02-18 14:51:43 +0000

    Removed DefaultBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new BDBStudios\BlogBundle\BDBStudiosBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
